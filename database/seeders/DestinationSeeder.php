6dac9c44bbae2cb5cf208cb8d54ae7bec86f200c
faizirs <ijayfaizi@gmail.com>
2023-11-22 08:49:12 +0700

    migrations

---
+++ database/seeders/DestinationSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DestinationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $destinations = ([
+            ([
+                "nama" => "Makam Sunan Gunung Jati",
+                "location" => "Gunung Jati",
+                "details" => "Pemakaman",
+                "day_open" => "Setiap Hari",
+                "time_open" => "07:00 WIB",
+                "pricing" => "Gratis",
+            ]),
+            ([
+                "nama" => "Waterpark Ciperna",
+                "location" => "Ciperna",
+                "details" => "Kolam Renang",
+                "day_open" => "Setiap Hari",
+                "time_open" => "07:00 WIB",
+                "pricing" => "Berbayar",
+            ])
+        ]);
+
+        Pegawai::insert($destinations);
+    }
+}
